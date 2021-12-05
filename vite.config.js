66b1a642261b0ca205a0346247a2dc92967ad315
funstra <founstra@gmail.com>
2021-12-05 23:11:38 +0100

    second

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { resolve } from 'path'
 export default defineConfig({
     resolve: {
         alias: {
-            "@": resolve(__dirname, 'src'),
-            "@:L": resolve(__dirname, 'src/lib'),
+            "!": resolve(__dirname, 'src'),
+            "!L": resolve(__dirname, 'src/lib'),
         }
     }
 })
